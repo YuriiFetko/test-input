7a66ca7164debd9e3f2e528da06494515dcef12c
Yurii <yurii.f@gmail.com>
2021-02-02 15:28:31 +0200

    test

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,7 +1,23 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import {Store} from '@ngrx/store';
 import * as fromRoot from '../store';
 import * as taskAction from '../store/task-list.actions';
+// @ts-ignore
+import {ListKeyManager} from '@angular/cdk/a11y';
+// @ts-ignore
+import {UP_ARROW, DOWN_ARROW, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-task-list',
@@ -10,11 +26,31 @@ import * as taskAction from '../store/task-list.actions';
 })
 export class TaskListComponent implements OnInit {
 
-  constructor() {
-  }
-
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  clickInput() {
 
+  }
+
+  // tslint:disable-next-line:typedef
+  inputValue(e: Event) {
+    console.log(e);
+  }
+
+  // tslint:disable-next-line:typedef
+  handleKeydown(e: KeyboardEvent) {
+
+  }
+
+  // tslint:disable-next-line:typedef
+  keypPress() {
+
+  }
+
+  // tslint:disable-next-line:typedef
+  onInputKeydown(e: any) {
+    alert('enter');
+  }
 }
