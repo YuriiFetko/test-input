c3e7fce4d1309bd5c13d4b8db2eaa789357ef466
Yurii <fetkoyura@gmail.com>
2021-03-04 10:47:21 +0200

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import {StoreModule} from '@ngrx/store';
 import {reducers} from './store/index';
 import {TaskListComponent} from './task-list/task-list.component';
 import {FormsModule} from '@angular/forms';
+import {PinchZoomModule} from "ngx-pinch-zoom";
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        StoreModule.forRoot(reducers),
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    FormsModule,
+    PinchZoomModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
